cd62a06183de3ff0a8f96f9a394dc88d5295d5c9
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-02-19 15:49:35 -0700

    Redoing line break tests to account properly for paragraph tests.

--- chisel.rb
+++ chisel.rb
@@ -1,21 +1,27 @@
 class Chisel
-  attr_reader :document, :lines
+  attr_reader :document, :lines  # => nil
 
   def initialize(document)
-    @document = document
+    @document = document    # => "#Here is my document.\n Here is another element"
   end
 
+#break it up line by line
   def line_break
-    arry_of_lines = []
-    arry_of_lines << @document.split("\n")
-    arry_of_lines.map do |line|
-      line.to_s
-    end
+    @lines = @document.split("\n")  # => ["#Here is my document.", " Here is another element"]
   end
+#see what that line starts with #####, ####, ###, ##, #, or /n
 
   def punc_sub
-
-    @document.gsub('#', '<h1>') + '</h1>'
+    @document = "#{document.gsub("#", "<h1>")}</h1>"
   end
 
 end
+
+parser = Chisel.new("#Here is my document.\n Here is another element")  # => #<Chisel:0x007ff49991a768 @document="#Here is my document.\n Here is another element">
+parser.line_break                                                       # => ["#Here is my document.", " Here is another element"]
+puts parser.lines[0]                                                       # => nil
+
+
+
+# >> #Here is my document.
+# >>  Here is another element
--- chisel_test.rb
+++ chisel_test.rb
@@ -1,7 +1,7 @@
-require 'minitest'
-require 'minitest/autorun'
-require 'minitest/pride'
-require_relative 'chisel'
+require 'minitest'          # => true
+require 'minitest/autorun'  # => true
+require 'minitest/pride'    # => true
+require_relative 'chisel'   # ~> SyntaxError: /Users/pmedaugh/Documents/Turing/Chisel/chisel.rb:30: syntax error, unexpected end-of-input, expecting keyword_end
 
 class ChiselTest < Minitest::Test
 
@@ -19,9 +19,27 @@ class ChiselTest < Minitest::Test
     assert_equal "<h1>This is a header as far as Markdown is concerned</h1>", parser.punc_sub
   end
 
-  def test_it_can_parse_out_lines_of_text
-    parser = Chisel.new("This is more than one \n line of text.")
-    assert_equal "<p>This is more than one</p> \n <p>line of text</p>", parser.line_break
+  def test_it_can_look_at_the_document_line_by_line
+    parser = Chisel.new("This is a line \nthis is also a line.")
+    parser.line_break
+    assert_equal "this is also a line.", parser.lines[1]
   end
 
+
 end
+
+# >> Run options: --seed 36026
+# >>
+# >> # Running:
+# >>
+# >>
+# >>
+# >> [31mF[0m[32ma[0m[33mb[0m[34mu[0m[35ml[0m[36mo[0m[31mu[0m[32ms[0m[33m [0m[34mr[0m[35mu[0m[36mn[0m in 0.002306s, 0.0000 runs/s, 0.0000 assertions/s.
+# >>
+# >> 0 runs, 0 assertions, 0 failures, 0 errors, 0 skips
+
+# ~> SyntaxError
+# ~> /Users/pmedaugh/Documents/Turing/Chisel/chisel.rb:30: syntax error, unexpected end-of-input, expecting keyword_end
+# ~>
+# ~> /Users/pmedaugh/Documents/Turing/Chisel/chisel_test.rb:4:in `require_relative'
+# ~> /Users/pmedaugh/Documents/Turing/Chisel/chisel_test.rb:4:in `<main>'
